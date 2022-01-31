2bccb5267a7715d974f51f3e70f6866495f65e5e
Hanh <hanh425@gmail.com>
2022-01-31 10:25:45 +0800

    Fix for ycash canopy

--- src/coindef/ycash.rs
+++ src/coindef/ycash.rs
@@ -1,7 +1,7 @@
-use zcash_primitives::consensus::{BranchId, Network};
+use zcash_primitives::consensus::{BlockHeight, BranchId, Network};
 
 pub const NETWORK: Network = Network::YCashMainNetwork;
 pub const TICKER: &str = "ycash";
-pub fn get_branch(_height: u32) -> BranchId {
-    BranchId::Ycash
+pub fn get_branch(height: u32) -> BranchId {
+    BranchId::for_height(&NETWORK, BlockHeight::from_u32(height))
 }
