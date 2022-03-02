7cd13f96cd9096aebf6759fd2de42677418ee813
Hanh <hanh425@gmail.com>
2022-03-02 23:44:21 +0800

    Unification

--- src/coin.rs
+++ src/coin.rs
@@ -1,7 +1,55 @@
 use zcash_primitives::consensus::{BlockHeight, BranchId, Network};
+use serde::{Serialize, Deserialize};
 
-pub const NETWORK: Network = Network::MainNetwork;
-pub const TICKER: &str = "zcash";
-pub fn get_branch(height: u32) -> BranchId {
-    BranchId::for_height(&NETWORK, BlockHeight::from_u32(height))
+#[derive(Copy, Clone, Serialize, Deserialize, Debug)]
+pub enum CoinType {
+    Ycash, Zcash,
 }
+
+pub fn get_coin_type(coin: u8) -> CoinType {
+    match coin {
+        0 => CoinType::Zcash,
+        1 => CoinType::Ycash,
+        _ => CoinType::Zcash,
+    }
+}
+
+struct YCASH;
+struct ZCASH;
+
+pub fn get_coin_chain(c: CoinType) -> &'static dyn CoinChain {
+    match c {
+        CoinType::Ycash => &YCASH,
+        CoinType::Zcash => &ZCASH,
+    }
+}
+
+pub trait CoinChain {
+    fn network(&self) -> &'static Network;
+    fn ticker(&self) -> &'static str;
+}
+
+impl CoinChain for YCASH {
+    fn network(&self) -> &'static Network {
+        &Network::YCashMainNetwork
+    }
+
+    fn ticker(&self) -> &'static str {
+        "ycash"
+    }
+}
+
+impl CoinChain for ZCASH {
+    fn network(&self) -> &'static Network {
+        &Network::MainNetwork
+    }
+
+    fn ticker(&self) -> &'static str {
+        "zcash"
+    }
+}
+
+pub fn get_branch(network: &Network, height: u32) -> BranchId {
+    BranchId::for_height(network, BlockHeight::from_u32(height))
+}
+
