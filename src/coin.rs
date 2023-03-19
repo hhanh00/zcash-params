2666e7052205cf0bcbea9e00f081e42fb1aa4281
Hanh <hanh425@gmail.com>
2023-03-19 17:20:34 +1000

    Remove CoinType

--- src/coin.rs
+++ src/coin.rs
@@ -1,45 +1,9 @@
 use zcash_primitives::consensus::{BlockHeight, BranchId, Network};
-use serde::{Serialize, Deserialize};
-
-#[derive(Copy, Clone, Serialize, Deserialize, Debug)]
-pub enum CoinType {
-    Ycash, Zcash, PirateChain,
-}
-
-impl Default for CoinType {
-    fn default() -> Self {
-        CoinType::Ycash
-    }
-}
-
-pub fn get_coin_type(coin: u8) -> CoinType {
-    match coin {
-        0 => CoinType::Zcash,
-        1 => CoinType::Ycash,
-        2 => CoinType::PirateChain,
-        _ => CoinType::Zcash,
-    }
-}
-
-pub fn get_coin_id(coin: CoinType) -> u8 {
-    match coin {
-        CoinType::Zcash => 0,
-        CoinType::Ycash => 1,
-        CoinType::PirateChain => 2,
-    }
-}
 
 struct YCASH;
 struct ZCASH;
 struct PIRATECHAIN;
-
-pub fn get_coin_chain(c: CoinType) -> &'static (dyn CoinChain + Send) {
-    match c {
-        CoinType::Ycash => &YCASH,
-        CoinType::Zcash => &ZCASH,
-        CoinType::PirateChain => &PIRATECHAIN,
-    }
-}
+struct OTHER;
 
 pub trait CoinChain: Send + Sync {
     fn network(&self) -> &'static Network;
@@ -57,21 +21,31 @@ impl CoinChain for YCASH {
         "ycash"
     }
 
-    fn has_transparent(&self) -> bool { true }
+    fn has_transparent(&self) -> bool {
+        true
+    }
 
-    fn has_unified(&self) -> bool { false }
+    fn has_unified(&self) -> bool {
+        false
+    }
 }
 
 impl CoinChain for ZCASH {
-    fn network(&self) -> &'static Network { &Network::MainNetwork }
+    fn network(&self) -> &'static Network {
+        &Network::MainNetwork
+    }
 
     fn ticker(&self) -> &'static str {
         "zcash"
     }
 
-    fn has_transparent(&self) -> bool { true }
+    fn has_transparent(&self) -> bool {
+        true
+    }
 
-    fn has_unified(&self) -> bool { true }
+    fn has_unified(&self) -> bool {
+        true
+    }
 }
 
 impl CoinChain for PIRATECHAIN {
@@ -83,11 +57,41 @@ impl CoinChain for PIRATECHAIN {
         "pirate-chain"
     }
 
-    fn has_transparent(&self) -> bool { false }
+    fn has_transparent(&self) -> bool {
+        false
+    }
 
-    fn has_unified(&self) -> bool { false }
+    fn has_unified(&self) -> bool {
+        false
+    }
+}
+
+impl CoinChain for OTHER {
+    fn network(&self) -> &'static Network {
+        unimplemented!()
+    }
+
+    fn ticker(&self) -> &'static str {
+        unimplemented!()
+    }
+
+    fn has_transparent(&self) -> bool {
+        true
+    }
+
+    fn has_unified(&self) -> bool {
+        false
+    }
 }
 
 pub fn get_branch(network: &Network, height: u32) -> BranchId {
     BranchId::for_height(network, BlockHeight::from_u32(height))
+}
+
+pub fn get_coin_chain(coin: u8) -> &'static (dyn CoinChain + Send) {
+    match coin {
+        0 => &ZCASH,
+        1 => &YCASH,
+        _ => &OTHER,
+    }
 }
